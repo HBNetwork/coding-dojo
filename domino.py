585311e60e6465179e6b5f2901abed35685142dd
fredericofavaro <fredericofavaro@gmail.com>
2023-12-01 00:39:30 -0300

    Continuando desafio 29112023

--- domino.py
+++ domino.py
@@ -27,73 +27,76 @@ Participantes:
 
 import pytest
 
-# def joga_domino(pedras):
-#   #
-#   # [3,2] - [2,1](3,1)
-#   # 1,2 ->
-#   for pedra_atual in pedras:
-#     for numero in pedra_atual:
-
-#   n = 0
-#   pedra_atual = pedras[n]
-#   pedras.pop(n)
-
-#   #pedra_seguinte = pedra_atual[n+1]
-#   resposta = pedra_atual
-
-#   for numero in pedra_atual:
-#       if numero == pedra_seguinte[0]:
-#           resposta.append(pedra_seguinte[0])
-
-#       if pedra_atual[0] == pedra_seguinte[-1]:
-#           resposta = 0
-
-#   return True
-
-
 def domino(pedras):
+    i = 0
+    respostas_validas = []
+
+    while i < len(pedras):
+        j = 0
+        while j <= 2**len(pedras):
+
+            pedra_inicial = pedras[0]
+            pedras_restantes = pedras[1:]
+
+            if pedra_inicial[0] > pedra_inicial[1]:
+                pedra_inicial = pedra_inicial[::-1]
+
+            ponta_esquerda = pedra_inicial[0]  #2
+            ponta_direita = pedra_inicial[1]  #1
+
+            resposta = []
+            resposta.append(pedra_inicial)
+
+            for pedra_atual in pedras_restantes:
+                if pedra_atual[0] == ponta_esquerda:
+                    ponta_esquerda = pedra_atual[1]
+                    resposta.insert(0, pedra_atual[::-1])
+                elif pedra_atual[0] == ponta_direita:
+                    ponta_direita = pedra_atual[1]
+                    resposta.append(pedra_atual)
+                elif pedra_atual[1] == ponta_esquerda:
+                    ponta_esquerda = pedra_atual[0]
+                    resposta.insert(0, pedra_atual)
+                elif pedra_atual[1] == ponta_direita:
+                    ponta_direita = pedra_atual[0]
+                    resposta.append(pedra_atual[::-1])
+
+            if ponta_esquerda == ponta_direita and resposta not in respostas_validas:
+                respostas_validas.append(resposta)
+
+            print(f'\n{pedras}')
+            j += 1
+            pedras = inverte_pedras(pedras)
+
+        pedras = permuta(pedras)
+        i += 1
+
+    print(f'\n{respostas_validas}')
+    return respostas_validas
+
+
+def permuta(pedras):
     pedra_inicial = pedras[0]
-    pedras_restantes = pedras[1:]
-
-    if pedra_inicial[0] > pedra_inicial[1]:
-        pedra_inicial = pedra_inicial[::-1]
-
-    ponta_esquerda = pedra_inicial[0]  #2
-    ponta_direita = pedra_inicial[1]  #1
-
-    resposta = []
-    resposta.append(pedra_inicial)
-
-    for pedra_atual in pedras_restantes:
-        if pedra_atual[0] == ponta_esquerda:
-            ponta_esquerda = pedra_atual[1]
-            #resposta.append(pedra_atual[::-1])
-            resposta.insert(0,pedra_atual[::-1])
-        elif pedra_atual[0] == ponta_direita:
-            ponta_direita = pedra_atual[1]
-            resposta.append(pedra_atual)
-        elif pedra_atual[1] == ponta_esquerda:
-            ponta_esquerda = pedra_atual[0]
-            #resposta.append(pedra_atual)
-            resposta.insert(0,pedra_atual)
-        elif pedra_atual[1] == ponta_direita:
-            ponta_direita = pedra_atual[0]
-            resposta.append(pedra_atual[::-1])
-
-    print(f'\n{ponta_esquerda} - {ponta_direita}')
-    return resposta
+    retorno = pedras[1:]
+    retorno.append(pedra_inicial)
+    return retorno
 
 
-def test_asdf():
-    assert domino([[2, 1], [2, 3], [1, 3]]) == [[3, 1], [1, 2], [2, 3]]
-    assert domino([[1,2], [4,1], [2,3]]) == [[4, 1], [1, 2], [2, 3]]
-    #assert domino([[1,2], [4,1], [2,3], [4,3]]) == [[3,2], [2,1], [1,4], [4,3]]
+def inverte_pedras(pedras):
+
+
+
+def test_domino():
+    assert domino([[2, 1], [2, 3], [1, 3]]) == [[[3, 1], [1, 2], [2, 3]], [[1, 2], [2, 3], [3, 1]], [[3, 2], [2, 1], [1, 3]]]
+    
+    #[[[1, 2], [2, 3], [3, 1]],[[3, 2], [2, 1], [1, 3]],[[3, 1], [1, 2], [2, 3]]]
+    #assert domino([[2, 3], [1, 3], [2, 1]]) == [[1, 2], [2, 3], [3, 1]]
+    #assert domino([[1, 2], [4, 1], [2, 3]]) == [[4, 1], [1, 2], [2, 3]]
+    #assert domino([[1, 2], [4, 1], [2, 3], [4, 3]]) == [[[1, 2], [2, 3], [3, 4], [4, 1]], [[3, 4], [4, 1], [1, 2], [2, 3]]]
     #assert domino([[4,1], [2,3], [4,3], [1,2]]) == [[3,2], [2,1], [1,4], [4,3]]
     #assert domino([[2,3], [4,3], [1,2], [4,1]]) == [[3,2], [2,1], [1,4], [4,3]]
     #assert domino([[4,3], [1,2], [4,1], [2,3]]) == [[3,2], [2,1], [1,4], [4,3]]
 
-
-#assert joga_domino([[2,1],[2,3],[1,3]]) == [[1,2],[2,3],[3,1]]
 
 if __name__ == "__main__":
     pytest.main(['-svv', __file__])
